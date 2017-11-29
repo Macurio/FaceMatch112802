923c2cc004b9b451722ca2a97513fd5e605a7d9b
Macurio <yxemfw@outlook.com>
2017-11-29 17:59:52 +0800

    17/11/29
    without face match , face library , face search , file io in form.

---
+++ FaceMatch112802/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FaceMatch112802
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Program.FaceDetect();
+            textBox1.Text = "age:"+Program.age.ToString() + "\nbeauty:"+Program.beauty.ToString()+"\n(beauty满分100，分数越高越美)";
+        }
+    }
+}
---
+++ FaceMatch112802/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+using Baidu.Aip.Face;
+
+namespace FaceMatch112802
+{
+    static class Program
+    {
+        
+
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); 
+        }
+        public static double age,beauty;
+        public static void FaceDetect()
+        {
+            var APP_ID = "10447843";
+            var API_KEY = "bHnKHuGkpQEaSU5k68F5MGen";
+            var SECRET_KEY = "EojiKc7KgHF6FySfgp9lZYPynjAonRp6 ";
+            var client = new Baidu.Aip.Face.Face(API_KEY, SECRET_KEY);
+            var image = File.ReadAllBytes("C:/Users/Administrator/Pictures/yangmi (3).jpg");
+            var options = new Dictionary<string, object>()
+    {
+        {"face_fields", "beauty,age"}
+    };
+            age = double.Parse(client.FaceDetect(image, options)["result"][0]["age"].ToString());
+            beauty = double.Parse(client.FaceDetect(image, options)["result"][0]["beauty"].ToString());
+
+        }
+    }
+}
+//有待添加的功能
+//上人脸库 人脸查找功能 
+//基本的体验优化 将每次的结果输入到txt文件或数据库中在本地保存 在窗体中打开图片
